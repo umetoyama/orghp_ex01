fa33bf3ef5109e419587f530b7fd73dd0bde7fac
umetoyama <dumeyama@gmail.com>
2022-02-05 15:23:27 +0900

    createアクション設定

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -8,6 +8,17 @@ class Users::RegistrationsController < Devise::RegistrationsController
     @user = User.new
   end
 
+  def create
+    @user = User.new(sign_up_params)
+      unless @user.valid?
+        render :new and return
+      end
+    session["devise.regist_data"] = { user: @user.attributes }
+    session["devise.regist_data"][:user][:password] = params[:user][:paaword]
+    @profile = @user.build_address
+    reder :new_profie
+  end
+
   # GET /resource/sign_up
   # def new
   #   super
