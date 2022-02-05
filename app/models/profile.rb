e9c4d60ce88f851691f45cc07dbdbc0c89c5c304
umetoyama <dumeyama@gmail.com>
2022-02-05 14:26:54 +0900

    アソシエーション設定

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,5 +1,9 @@
 class Profile < ApplicationRecord
 
+  belongs_to :user, optional: true
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :position
+
   validates :position_id, numericality: { other_than: 1, mesages: " can't be blank" }
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_one :profile
+
   PASSWORD_RULES = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
 
   validates :name, presence: true
