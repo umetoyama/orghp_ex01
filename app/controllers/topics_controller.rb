a903dac367d2f5b33ace522da929d496edc7b9d3
umetoyama <dumeyama@gmail.com>
2022-02-10 14:27:43 +0900

    バリデーション設定/ストロングパラメーター編集

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -16,7 +16,7 @@ class TopicsController < ApplicationController
   private
 
   def topic_params
-    params.require(:topic).permit(:tetle, :content, :image).merge(user_id: current_user.id)
+    params.require(:topic).permit(:tetle, :content).merge(user_id: current_user.id)
   end
 
   def move_to_root_path
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -3,4 +3,7 @@ class Topic < ApplicationRecord
   has_rich_text :content
   belongs_to :user
 
+  validates :title, presence: true
+  validates :content, presence: true
+
 end
