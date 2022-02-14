5b1bb2d4321227d0876c3a1101aaeacd2d795c69
umetoyama <dumeyama@gmail.com>
2022-02-14 13:59:19 +0900

    コントローラー作成/アクション（new ,create)記述

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,23 @@
+class ContactsController < ApplicationController
+
+  def new
+    @contact = Contact.new
+  end
+
+  def create
+    @contact = Contact.new
+    if @contact.save
+      ContactMailer.contact_mail(@contact).deliver
+      redirect_to new_contact_path
+    else
+      render: :new
+    end
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:name, :email, :message)
+  end
+
+end
