53eac4355097c466a57bea0e14e47e382fd7dad8
umetoyama <dumeyama@gmail.com>
2022-02-27 01:12:25 +0900

    fourthcontent(member)部分修正

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -2,7 +2,9 @@ class TopController < ApplicationController
 
   def index
     @topics = Topic.order(id: :DESC)
-    @topics_new = @topics.take(5)
+    @topics_new = @topics.take(4)
+    @profiles = Profile.order(position_id: :ASC)
+    @profile_select = @profiles.take(4)
   end
 
 end
