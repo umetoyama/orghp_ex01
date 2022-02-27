88c5c00f60e19c2e0a6895191a831c7ee2b2af2a
umetoyama <dumeyama@gmail.com>
2022-02-27 21:11:29 +0900

    content幅調整とそれによるレイアウトの調整/背景ボーダーの調整

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -2,9 +2,9 @@ class TopController < ApplicationController
 
   def index
     @topics = Topic.order(id: :DESC)
-    @topics_new = @topics.take(4)
+    @topics_new = @topics.take(3)
     @profiles = Profile.order(position_id: :ASC)
-    @profile_select = @profiles.take(4)
+    @profile_select = @profiles.take(3)
   end
 
 end
