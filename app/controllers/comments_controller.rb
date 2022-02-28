790f763faa24b5457e893c034702b3b67ef56470
umetoyama <dumeyama@gmail.com>
2022-03-01 00:47:28 +0900

    コメントコントローラー記述

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,16 @@
 class CommentsController < ApplicationController
+
+  def create
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      redirect_to topic_path(params[:topic_id])
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text).merge(user_id: current_user.id, topic_id: params[:topic_id])
+  end
+  
 end
