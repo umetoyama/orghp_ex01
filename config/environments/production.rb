d33f6684e9c33c51aa6687cfc54d8439f25845bf
umetoyama <dumeyama@gmail.com>
2022-03-08 19:44:39 +0900

    actioncable使用の為heroku上でも使用できる様記述

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,4 +109,9 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  ActionCable.server.config.disable_request_forgery_protection = true
+  config.action_cable.url = "wss://orghp_ex01.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://orghp_ex01.herokuapp.com/cable', 'http://orghp_ex01.herokuapp.com/cable']
+  
 end
